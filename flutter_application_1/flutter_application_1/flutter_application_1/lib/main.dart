cc1a18677bc37829ec25814da87c13f35c1dbfea
Sabina Abubakirova <bisb22-30@NTDOM.LOCAL>
2026-01-20 09:37:14 +0500

    lab_1

---
+++ flutter_application_1/flutter_application_1/flutter_application_1/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //    home: Text('Hello World')
+  //   );
+  // }
+    Widget build(BuildContext context) {
+    return MaterialApp(
+     home: Scaffold(appBar: AppBar(backgroundColor: Colors.pink,),
+     body: Container(color: Colors.purple, child: Text('jjjj'),),)
+    );
+  }
+}
+
